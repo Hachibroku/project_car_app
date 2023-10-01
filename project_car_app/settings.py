33c6db91be17cc456228fc6f168c89748ebb904b
Murphey Osmundson <mosmundson88@gmail.com>
2023-09-30 23:57:07 -0400

    Front end buildout using vite/react

--- project_car_app/settings.py
+++ project_car_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,10 +28,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',  # or the port your frontend is running on
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    "projects",
+    "rest_framework",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +62,7 @@ ROOT_URLCONF = 'project_car_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'vite-project/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'vite-project/dist'),
+]
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
