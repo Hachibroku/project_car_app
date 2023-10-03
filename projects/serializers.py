2d8929adaff613cd17dbcffcb0dc4cea7135cc75
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-03 19:56:18 -0400

    Updated vehicle serializer for Job foreignkey.

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
 from .models import Vehicle, Job, Part, Tool
 
-class VehicleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = '__all__'
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = '__all__'
 
+
+class VehicleSerializer(serializers.ModelSerializer):
+    jobs = JobSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Vehicle
+        fields = ['id', 'name', 'make', 'model', 'year', 'jobs']
+
+
 class PartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Part
         fields = '__all__'
+
 
 class ToolSerializer(serializers.ModelSerializer):
     class Meta:
