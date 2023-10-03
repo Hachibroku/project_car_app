891c34017073e8e02b71c42d70021e492fe65645
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-03 19:57:14 -0400

    Route added for vehicle detail.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import VehicleList from './components/VehicleList';
+import VehicleDetail from './components/VehicleDetail';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         </nav>
         <Routes>
           <Route path="/vehicles" element={<VehicleList />} />
+          <Route path="/vehicles/:vehicleId" element={<VehicleDetail />} />
           {/* Other routes */}
         </Routes>
       </div>
