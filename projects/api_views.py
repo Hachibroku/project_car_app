58d9c0042744a1691c26e844de008c3273e63f41
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-03 19:55:55 -0400

    Added class based views for parts and tools.

--- projects/api_views.py
+++ projects/api_views.py
@@ -12,12 +12,31 @@ class VehicleRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
 
-# Repeat the pattern for Job, Part, and Tool models.
 class JobListCreate(generics.ListCreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
 
 
+class PartListCreate(generics.ListCreateAPIView):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
+
+
+class PartRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
+
+
 class JobRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+
+class ToolListCreate(generics.ListCreateAPIView):
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
+
+
+class ToolRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tool.objects.all()
+    serializer_class = ToolSerializer
