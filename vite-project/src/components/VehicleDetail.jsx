7013ccd2f5fbc3dcc6748460d1a8c03a40cdae30
Murphey Osmundson <mosmundson88@gmail.com>
2023-10-29 13:43:56 -0400

    Removed redundant stylesheet.

--- vite-project/src/components/VehicleDetail.jsx
+++ vite-project/src/components/VehicleDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './VehicleDetail.css';
 
 function VehicleDetail() {
   const [vehicle, setVehicle] = useState(null);
